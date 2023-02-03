ccf6110915c2eac2baf9fa92b6649c3b8ec87d04
Gabcik <gab563riela@gmail.com>
2023-02-03 20:08:43 +0100

    Deleted unnecessary method from DAOConfig

--- bookstoreAPI/src/main/java/org/example/DAOConfig.java
+++ bookstoreAPI/src/main/java/org/example/DAOConfig.java
@@ -7,21 +7,4 @@ public class DAOConfig {
     public static final String DB_USER = "postgres";
     public static final String DB_PASS = "percia123";
 
-    public Connection connect() {
-
-        try {
-            Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
-            System.out.println("Connect!");
-
-            ResultSet rs = connection.createStatement().executeQuery("SELECT current_date as today");
-            while(rs.next()) {
-                System.out.println(rs.getDate("today"));
-            }
-
-            return connection;
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-
-    }
 }
